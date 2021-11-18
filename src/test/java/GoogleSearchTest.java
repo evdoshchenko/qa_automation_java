f418327958b9467086d784b7a6f94cd3cb975159
Danil <aka4j7@gmail.com>
2021-11-19 01:15:18 +0300

    parameters and browser setup

--- src/test/java/GoogleSearchTest.java
+++ src/test/java/GoogleSearchTest.java
@@ -4,13 +4,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+
 public class GoogleSearchTest {
+
+    WebDriver driver;
+
     @Test
-    public void testName() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+    public void testSearchAndVerifyResults() {
 
         driver.get("https://google.com");
 
@@ -27,4 +31,44 @@ public class GoogleSearchTest {
         Assert.assertTrue(parseInteger > 38000);
 
     }
+
+    @Parameters({"browser"})
+    @BeforeSuite
+    public void suitSetup(String browser) {
+
+        String os = System.getProperty("os.name");
+
+        if(os.contains("Windows")){
+            System.setProperty("webdriver.gecko.driver", "src/test/resources/windows/geckodriver.exe");
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/windows/chromedriver.exe");
+        }
+
+        if (browser.equalsIgnoreCase("Firefox")){
+            driver = new FirefoxDriver();
+        }
+        if (browser.equalsIgnoreCase("Chrome")){
+            driver = new ChromeDriver();
+        }
+
+
+
+    }
+
+    @Test
+    public void testOSVersion() {
+        String os = System.getProperty("os.name");
+        System.out.println(os);
+    }
+
+    @Test
+    public void testFirefox() {
+
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://google.com");
+
+    }
+
+
 }
